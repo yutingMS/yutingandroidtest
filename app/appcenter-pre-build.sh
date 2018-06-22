1f6f1cbc10c52232d3ced64a0d2fc96d9326b7f1
yutingMS <34529911+yutingMS@users.noreply.github.com>
2018-06-22 11:54:00 +0800

    Update appcenter-pre-build.sh

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -8,11 +8,11 @@ fi
 
 #APP_CONSTANT_FILE=$APPCENTER_SOURCE_DIRECTORY/RPS/Constants/AppConstant.cs
 
-if [ -e "$APP_CONSTANT_FILE" ]
-then
-    echo "Updating ApiUrl to $APP_CENTER_KEY in AppConstant.cs"
-    sed -i '' 's#AppCenterKey = "[a-z:./]*"#AppCenterKey = "'$APP_CENTER_KEY'"#' $APP_CONSTANT_FILE
-
-    echo "File content:"
-    cat $APP_CONSTANT_FILE
-fi
+#if [ -e "$APP_CONSTANT_FILE" ]
+#then
+#    echo "Updating ApiUrl to $APP_CENTER_KEY in AppConstant.cs"
+#    sed -i '' 's#AppCenterKey = "[a-z:./]*"#AppCenterKey = "'$APP_CENTER_KEY'"#' $APP_CONSTANT_FILE
+#
+#    echo "File content:"
+#    cat $APP_CONSTANT_FILE
+#fi
